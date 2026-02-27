759f2d4c78d760d7b6558f74ae3fda9542e8e9ee
Melfreitass <melissa.g.freitas@aluno.senai.br>
2026-02-27 11:12:40 -0300

    feat: cria pastas para CRUD de produtos

---
+++ src/models/produtoModel.js
@@ -0,0 +1,51 @@
+import prisma from '../utils/prismaClient.js';
+
+export default class ProdutoModel {
+    constructor({ id = null, nome, descricao, categoria, preco, disponivel = true } = {}) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.categoria = categoria;
+        this.preco = preco;
+        this.disponivel = disponivel;
+    }
+
+    async criar() {
+        return prisma.produto.create({
+            data: {
+                nome: this.nome,
+                descricao: this.descricao,
+                categoria: this.categoria,
+                preco: this.preco,
+                disponivel: this.disponivel
+            },
+        });
+    }
+
+    async atualizar() {
+        return prisma.exemplo.update({
+            where: { id: this.id },
+            data: { nome: this.nome, estatus: this.estatus, preco: this.preco },
+        });
+    }
+
+    async deletar() {
+        return prisma.exemplo.delete({ where: { id: this.id } });
+    }
+
+    static async buscarTodos(filtros = {}) {
+        const where = {};
+
+        if (filtros.nome) where.nome = { contains: filtros.nome, mode: 'insensitive' };
+        if (filtros.estatus !== undefined) where.estatus = filtros.estatus === 'true';
+        if (filtros.preco !== undefined) where.preco = parseFloat(filtros.preco);
+
+        return prisma.exemplo.findMany({ where });
+    }
+
+    static async buscarPorId(id) {
+        const data = await prisma.exemplo.findUnique({ where: { id } });
+        if (!data) return null;
+        return new ExemploModel(data);
+    }
+}
