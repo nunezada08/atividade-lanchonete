7d76ef126296fe652b7ee4abf81faf70defa6365
FelipeCampos_ <felipe.a.campos7@aluno.senai.br>
2026-02-27 11:52:14 -0300

    Add clienteRoutes for cliente CRUD operations

---
+++ src/routes/clienteRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import * as controller from '../controllers/clienteController.js';
+
+const router = express.Router();
+
+router.post('/cliente', controller.criar);
+router.get('/cliente', controller.buscarTodos);
+router.get('/cliente/:id', controller.buscarPorId);
+router.put('/cliente/:id', controller.atualizar);
+router.delete('/cliente/:id', controller.deletar);
+
+export default router;
